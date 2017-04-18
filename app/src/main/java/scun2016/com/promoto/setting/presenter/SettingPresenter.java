4721fea3be0905c817714233c9c0e039397a7646
EricLi <1057635338@qq.com>
2017-04-18 10:04:50 +0800

    【新增】修改setting界面

---
+++ app/src/main/java/scun2016/com/promoto/setting/presenter/SettingPresenter.java
@@ -0,0 +1,16 @@
+package scun2016.com.promoto.setting.presenter;
+
+import scun2016.com.promoto.setting.view.ISettingView;
+
+/**
+ * Created by EricLi.
+ * on 2017/4/18 in 上午9:37
+ * Email: EricLi1235@gmial.com
+ */
+
+public class SettingPresenter {
+    private ISettingView mSettingView;
+    public SettingPresenter(ISettingView settingView){
+        mSettingView = settingView;
+    }
+}
---
+++ app/src/main/java/scun2016/com/promoto/setting/view/ISettingView.java
@@ -0,0 +1,10 @@
+package scun2016.com.promoto.setting.view;
+
+/**
+ * Created by EricLi.
+ * on 2017/4/18 in 上午9:36
+ * Email: EricLi1235@gmial.com
+ */
+
+public class ISettingView {
+}
